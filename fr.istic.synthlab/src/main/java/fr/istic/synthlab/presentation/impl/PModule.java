da6e578a959ebcf5807743414678bf6305305531
romfret <romfret@gmail.com>
2013-02-05 02:35:57 +0100

    PModule ameliore

--- fr.istic.synthlab/src/main/java/fr/istic/synthlab/presentation/impl/PModule.java
+++ fr.istic.synthlab/src/main/java/fr/istic/synthlab/presentation/impl/PModule.java
@@ -2,9 +2,10 @@ package fr.istic.synthlab.presentation.impl;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.FlowLayout;
 import java.util.List;
 
+import javax.swing.BoxLayout;
+import javax.swing.JPanel;
 import javax.swing.event.AncestorEvent;
 import javax.swing.event.AncestorListener;
 
@@ -24,6 +25,9 @@ public class PModule extends WebInternalFrame implements IPModule {
 
 	private static final long serialVersionUID = -8519084219674310285L;
 	private ICModule ctrl;
+	private JPanel panelParameters;
+	private JPanel panelInputPorts;
+	private JPanel panelOutputPorts;
 	private int width;
 	private int height;
 	
@@ -40,8 +44,14 @@ public class PModule extends WebInternalFrame implements IPModule {
 
 	private void configView() {
 		this.setBackground(Color.GRAY);
-		this.setLayout(new FlowLayout());
+		panelParameters = new JPanel();
+		panelInputPorts = new JPanel();
+		panelOutputPorts = new JPanel();
+		this.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.PAGE_AXIS));
 		this.setAutoscrolls(true);
+		this.getContentPane().add(panelParameters,0);
+		this.getContentPane().add(panelInputPorts,1);
+		this.getContentPane().add(panelOutputPorts,2);
 
 		width = 350;
 		height = 350;
@@ -58,7 +68,6 @@ public class PModule extends WebInternalFrame implements IPModule {
 	            List<ICWire> wires = ctrl.getWires();
 	            for(ICWire wire : wires){
 	            	if(wire!=null){
-	            		System.out.println("wire="+wire);
 	            		wire.getPresentation().updateDisplay();
 	            	}
 	            }
@@ -84,18 +93,18 @@ public class PModule extends WebInternalFrame implements IPModule {
 
 	@Override
 	public void addInputPort(IPInputPort presentation) {
-		add((PInputPort) presentation);
+		panelInputPorts.add((PInputPort) presentation);
 
 	}
 
 	@Override
 	public void addOutputPort(IPOutputPort presentation) {
-		add((POutputPort) presentation);
+		panelOutputPorts.add((POutputPort) presentation);
 	}
 
 	@Override
 	public void addParameter(IPParameter presentation) {
-		add((Component) presentation);
+		panelParameters.add((Component) presentation);
 	}
 
 }
