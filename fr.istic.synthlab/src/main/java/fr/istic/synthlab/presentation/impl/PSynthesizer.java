dcbc5af7d8426efbe2d2336d9ea679697d9b33ee
Raphaël Chevallier <slawek.web@gmail.com>
2013-02-09 17:19:18 +0100

    Update cast component onClick PSynthesizer

--- fr.istic.synthlab/src/main/java/fr/istic/synthlab/presentation/impl/PSynthesizer.java
+++ fr.istic.synthlab/src/main/java/fr/istic/synthlab/presentation/impl/PSynthesizer.java
@@ -28,17 +28,16 @@ public class PSynthesizer extends WebDesktopPane implements IPSynthesizer {
 	private ICSynthesizer ctrl;
 
 	private List<IPModule> modules;
-	
-	
+
 	public PSynthesizer(ICSynthesizer control) {
 		super();
 		ctrl = control;
 		modules = new ArrayList<IPModule>();
-		
+
 		configView();
 		defineCallbacks();
 	}
-	
+
 	private void configView() {
 		this.setBackground(Color.DARK_GRAY);
 	}
@@ -50,51 +49,65 @@ public class PSynthesizer extends WebDesktopPane implements IPSynthesizer {
 			public void mouseClicked(MouseEvent e) {
 				int x = e.getPoint().x;
 				int y = e.getPoint().y;
-				e.translatePoint(-(PSynthesizer.this.getComponentAt(x, y).getLocation().x), -(PSynthesizer.this.getComponentAt(x, y).getLocation().y));
-				
-				JPanel panelPorts = (JPanel) ((JInternalFrame)getComponentAt(x,y)).getContentPane().getComponentAt(e.getPoint());
-				e.translatePoint(-panelPorts.getLocation().x, -panelPorts.getLocation().y);
-				
-					Component panelPort = panelPorts.getComponentAt(e.getPoint());
-					while(panelPort!=null){
-						e.translatePoint(-panelPort.getLocation().x, -panelPort.getLocation().y);
-						if((panelPort.getComponentAt(e.getPoint()) != null) && (panelPort.getComponentAt(e.getPoint()) instanceof JPanel)){
+				e.translatePoint(
+						-(PSynthesizer.this.getComponentAt(x, y).getLocation().x),
+						-(PSynthesizer.this.getComponentAt(x, y).getLocation().y));
+
+				Component clickedComponent = getComponentAt(x, y);
+				if (clickedComponent != null
+						&& JInternalFrame.class.isInstance(clickedComponent)) {
+					JPanel panelPorts = (JPanel) ((JInternalFrame) clickedComponent).getContentPane()
+							.getComponentAt(e.getPoint());
+					e.translatePoint(-panelPorts.getLocation().x,
+							-panelPorts.getLocation().y);
+
+					Component panelPort = panelPorts.getComponentAt(e
+							.getPoint());
+					while (panelPort != null) {
+						e.translatePoint(-panelPort.getLocation().x,
+								-panelPort.getLocation().y);
+						if ((panelPort.getComponentAt(e.getPoint()) != null)
+								&& (panelPort.getComponentAt(e.getPoint()) instanceof JPanel)) {
 							panelPort = panelPort.getComponentAt(e.getPoint());
 						} else {
 							break;
 						}
 					}
-					
-				panelPort.dispatchEvent(e);
+
+					panelPort.dispatchEvent(e);
+				}
 			}
 		});
-		
+
 		addMouseMotionListener(new MouseMotionListener() {
-			
+
 			@Override
 			public void mouseMoved(MouseEvent e) {
 				// Gestion du currentWire
-				if(ctrl.getCurrentWire() != null){
+				if (ctrl.getCurrentWire() != null) {
 					IInputPort input = ctrl.getCurrentWire().getInput();
 					IOutputPort output = ctrl.getCurrentWire().getOutput();
-					
+
 					Point mouse = getMousePosition(true);
-					
-					if(input == null && output != null){
-						((ICWire)getControl().getCurrentWire()).getPresentation().setInputPoint(mouse);
+
+					if (input == null && output != null) {
+						((ICWire) getControl().getCurrentWire())
+								.getPresentation().setInputPoint(mouse);
 					}
-					if(output == null && input != null){
-						((ICWire)getControl().getCurrentWire()).getPresentation().setOutputPoint(mouse);
+					if (output == null && input != null) {
+						((ICWire) getControl().getCurrentWire())
+								.getPresentation().setOutputPoint(mouse);
 					}
 				}
 			}
-			
+
 			@Override
-			public void mouseDragged(MouseEvent e) {}
+			public void mouseDragged(MouseEvent e) {
+			}
 		});
-		
+
 	}
-	
+
 	@Override
 	public ICSynthesizer getControl() {
 		return ctrl;
@@ -135,27 +148,29 @@ public class PSynthesizer extends WebDesktopPane implements IPSynthesizer {
 	@Override
 	public void c2pAddModule(IPModule module) {
 		((APModule) module).setVisible(true);
-		this.add((JInternalFrame) module,0);
+		this.add((JInternalFrame) module, 0);
 
-		((APModule)module).setBounds(10, 10, module.getWidth(), module.getHeight());
-		
+		((APModule) module).setBounds(10, 10, module.getWidth(),
+				module.getHeight());
+
 		modules.add(module);
-		((APModule)module).validate();
-		((APModule)module).repaint();
-		
+		((APModule) module).validate();
+		((APModule) module).repaint();
+
 		validate();
 		repaint();
 	}
 
 	@Override
 	public void c2pAddModuleOk(IPModule module) {
-		
+
 	}
-	
-	int i=0;
+
+	int i = 0;
+
 	@Override
 	public void c2pAddWire(IPWire wire) {
-		
+
 		this.add((PWire) wire);
 		System.out.println("c2pAddWire Adding wire");
 		validate();
@@ -171,15 +186,18 @@ public class PSynthesizer extends WebDesktopPane implements IPSynthesizer {
 
 	@Override
 	public void removeWire(IPWire pres) {
-		this.remove((PWire)pres);
+		this.remove((PWire) pres);
 		validate();
 		repaint();
 	}
 
-	public void clickAt(int x, int y,MouseEvent e ) {
-		e.translatePoint(-(this.getComponentAt(x, y).getLocation().x), -(this.getComponentAt(x, y).getLocation().y));
-		JPanel panelPorts = (JPanel) ((JInternalFrame)getComponentAt(x,y)).getContentPane().getComponentAt(e.getPoint());
-		e.translatePoint(-panelPorts.getLocation().x, -panelPorts.getLocation().y);
+	public void clickAt(int x, int y, MouseEvent e) {
+		e.translatePoint(-(this.getComponentAt(x, y).getLocation().x), -(this
+				.getComponentAt(x, y).getLocation().y));
+		JPanel panelPorts = (JPanel) ((JInternalFrame) getComponentAt(x, y))
+				.getContentPane().getComponentAt(e.getPoint());
+		e.translatePoint(-panelPorts.getLocation().x,
+				-panelPorts.getLocation().y);
 		JPanel panelPort = (JPanel) panelPorts.getComponentAt(e.getPoint());
 		e.translatePoint(-panelPort.getLocation().x, -panelPort.getLocation().y);
 		panelPort.dispatchEvent(e);
