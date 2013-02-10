d0a25c76e281d0a845685e6eca32207aba194f81
ClementH <clement.hardouin@gmail.com>
2013-02-10 11:35:01 +0100

    Ajout d'un test de conversion dB <-> volts

---
+++ fr.istic.synthlab/src/test/java/fr/istic/synthlab/abstraction/ConvertTest.java
@@ -0,0 +1,23 @@
+package fr.istic.synthlab.abstraction;
+
+import fr.istic.synthlab.abstraction.util.Convert;
+import junit.framework.TestCase;
+
+public class ConvertTest extends TestCase {
+
+	/**
+	 * Test the conversion volt/dB
+	 */
+	public void testConvertVolt2DB() {
+		double voltValue = 3.14;
+		assertEquals(voltValue, Convert.dB2V(Convert.v2Db(voltValue)));
+	}
+
+	/**
+	 * Test the conversion dB/volt
+	 */
+	public void testConvertDB2Volt() {
+		double dbValue = -12;
+		assertEquals(dbValue, Convert.v2Db(Convert.dB2V(dbValue)));
+	}
+}
