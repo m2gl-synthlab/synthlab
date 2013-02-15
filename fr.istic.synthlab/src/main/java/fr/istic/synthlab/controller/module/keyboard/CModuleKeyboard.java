b44bd939822363a83d81fc60feb19582c19ec589
ClementH <clement.hardouin@gmail.com>
2013-02-15 11:44:39 +0100

    Début création d'un piano

---
+++ fr.istic.synthlab/src/main/java/fr/istic/synthlab/controller/module/keyboard/CModuleKeyboard.java
@@ -0,0 +1,26 @@
+package fr.istic.synthlab.controller.module.keyboard;
+
+import fr.istic.synthlab.abstraction.module.keyboard.ModuleKeyboard;
+import fr.istic.synthlab.presentation.module.keyboard.IPModuleKeyboard;
+
+public class CModuleKeyboard extends ModuleKeyboard implements ICModuleKeyboard{
+
+	@Override
+	public IPModuleKeyboard getPresentation() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void p2cClosing() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setParameter(String key, Double value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
