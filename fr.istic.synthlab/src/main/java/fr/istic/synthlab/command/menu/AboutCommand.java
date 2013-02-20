4c1b1b3042b95dbf761175741e535c1e42346489
Raphaël Chevallier <slawek.web@gmail.com>
2013-02-20 10:21:01 +0100

    Edit my Name with 2 L ^^

--- fr.istic.synthlab/src/main/java/fr/istic/synthlab/command/menu/AboutCommand.java
+++ fr.istic.synthlab/src/main/java/fr/istic/synthlab/command/menu/AboutCommand.java
@@ -8,7 +8,7 @@ import fr.istic.synthlab.command.ICommand;
 public class AboutCommand implements ICommand {
 
 	public void execute() {
-		String html = "<html><body><div align=\"center\">SynthLab (Groupe 2)<br/> Chevalier - Chevalot - Guerra <br/> Hardouin - Kwete - Le Ho <br/> ISTIC 2013 </div></body></html>";
+		String html = "<html><body><div align=\"center\">SynthLab (Groupe 2)<br/> Chevallier - Chevalot - Guerra <br/> Hardouin - Kwete - Le Ho <br/> ISTIC 2013 </div></body></html>";
 		JOptionPane.showMessageDialog(new JFrame("About"), html, "About", JOptionPane.INFORMATION_MESSAGE);
 	}
 }
